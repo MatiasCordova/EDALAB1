bb0ffa064017f74f8a9990a29ce858117dc1f195
MatiasCordova <114171764+MatiasCordova@users.noreply.github.com>
2022-09-22 14:04:43 -0300

    Update ejercicioB.java

--- ejercicioB.java
+++ ejercicioB.java
@@ -1,78 +1,87 @@
+import java.util.Scanner;
 import java.util.*;
 
+public class Lista {
 
-public class ejercicioB {
-    static class Node
-{
-    int data;
-    Node next;   //creamos un nodo
-}
- static Node head = null;
+    List head;
 
-static Node Voltear(Node head, int n){
-    
-    Stack<Node> stack = new Stack<Node> ();
-    Node current = head;
-    Node prev = null;
- 
-    while (current != null){
-        int count = 0;
-        while (current != null && count < k)
-        {
-            stack.push(current);
-            current = current.next;
-            count++;
+    static class List {
+
+        int data;
+        List next;
+
+        List(int data, List next) {
+            this.data = data;
+            this.next = next;
         }
-        while (stack.size() > 0){
-            if (prev == null)
-            {
-                prev = stack.peek();
-                head = prev;
-                stack.pop();
-            }
-            else
-            {
-                prev.next = stack.peek();
-                prev = prev.next;
-                stack.pop();
-            }
+
+        List(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
-    prev.next = null;
- 
-    return head;
-}
-static void push( int new_data){
-    Node new_node = new Node();
-    new_node.data = new_data;
-    new_node.next = head;
-    head = new_node;
-}
-static void printList(Node node)
-{
-    while (node != null)
-    {
-        System.out.print(node.data + " ");
-        node = node.next;
-    }
-}
 
-public static void main(String[] args){
-    push( 9);
-    push( 8);
-    push( 7);
-    push( 6);
-    push( 5);
-    push(4);
-    push(3);
-    push(2);
-    push( 1);
- 
-    printList(head);
-    head = Voltear(head, 3);
-    System.out.println();
- 
-    System.out.println(" Linked list Invertida ");
-    printList(head);
-}
+    static public List GrupoAlReves(List head) {
+        if (head == null) {
+            return null;
+        }
+        List aux = head;
+        int count = 3;
+        while (aux != null && count > 0) {
+            aux = aux.next;
+            count--;
+        }
+        List last = null;
+
+        if (count > 0) {
+            return head;
+        } else {
+            last = GrupoAlReves(aux);
+        }
+        count = 3;
+
+        while (head != null && count > 0) {
+
+            List temp = head.next;
+            head.next = last;
+            last = head;
+            head = temp;
+            count--;
+        }
+        return last;
+    }
+
+    void push(int data) {
+        List aux = new List(data);
+        aux.data = data;
+        aux.next = head;
+        head = aux;
+    }
+
+    static void printList(List node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Lista l1 = new Lista();
+        Scanner sc = new Scanner(System.in);
+        int n, k;
+        System.out.println("Cuantos nodos desea ingresar: ");
+        n = sc.nextInt();
+        System.out.println("Ingrese los nodos ");
+        for (int i = 0; i < n; i++) {
+            k = sc.nextInt();
+            l1.push(k);
+        }
+        l1.printList(l1.head);
+        l1.head = GrupoAlReves(l1.head);
+        System.out.println();
+
+        System.out.println(" Linked list Invertida ");
+        printList(l1.head);
+
+    }
 }
